a800f29f2cd8d37113b0df0177c5ca7d61b5495d
Teddy Papadopoulos <tpapadop@opentext.com>
2019-04-26 17:25:17 -0400

    first

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// start the server
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Hello world! Listening on port ${port}`));
